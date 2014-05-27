6679976977c5602dff744d033d948d35c3adf84e
devknight216 <devknight216@gmail.com>
2014-05-27 11:25:20 -0700

    Fix issue #19: Bump requirements to use django-rest-framework 2.3.11 for write_only field support

--- rest_framework_jwt/serializers.py
+++ rest_framework_jwt/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 from rest_framework_jwt.settings import api_settings
@@ -12,24 +12,17 @@ class JSONWebTokenSerializer(serializers.Serializer):
     """
     Serializer class used to validate a username and password.
 
-    'username' is identified by the custom UserModel.USERNAME_FIELD.
-
     Returns a JSON Web Token that can be used to authenticate later calls.
     """
-
-    username_field = get_user_model().USERNAME_FIELD
+    username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
-    def __init__(self, *args, **kwargs):
-        """Dynamically add the USERNAME_FIELD to self.fields."""
-        super(JSONWebTokenSerializer, self).__init__(*args, **kwargs)
-        self.fields[self.username_field] = serializers.CharField()
-
     def validate(self, attrs):
-        credentials = {self.username_field: attrs.get(self.username_field),
-                       'password': attrs.get('password')}
-        if all(credentials.values()):
-            user = authenticate(**credentials)
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
 
             if user:
                 if not user.is_active:
