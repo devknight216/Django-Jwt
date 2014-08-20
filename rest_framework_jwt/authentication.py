0853cebbf6a4323022c50eba26c38bee6601bafc
devknight216 <devknight216@gmail.com>
2014-08-20 13:58:57 -0400

    Add classifiers

--- rest_framework_jwt/authentication.py
+++ rest_framework_jwt/authentication.py
@@ -13,7 +13,6 @@ else:
 
 
 jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
-jwt_get_user_id_from_payload = api_settings.JWT_PAYLOAD_GET_USER_ID_HANDLER
 
 
 class JSONWebTokenAuthentication(BaseAuthentication):
@@ -63,7 +62,7 @@ class JSONWebTokenAuthentication(BaseAuthentication):
         Returns an active user that matches the payload's user id and email.
         """
         try:
-            user_id = jwt_get_user_id_from_payload(payload)
+            user_id = payload.get('user_id')
 
             if user_id:
                 user = User.objects.get(pk=user_id, is_active=True)
--- rest_framework_jwt/settings.py
+++ rest_framework_jwt/settings.py
@@ -16,18 +16,12 @@ DEFAULTS = {
     'JWT_PAYLOAD_HANDLER':
     'rest_framework_jwt.utils.jwt_payload_handler',
 
-    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-
     'JWT_SECRET_KEY': settings.SECRET_KEY,
     'JWT_ALGORITHM': 'HS256',
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
-
-    'JWT_ALLOW_TOKEN_RENEWAL': False,
-    'JWT_TOKEN_RENEWAL_LIMIT': datetime.timedelta(days=7),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300)
 }
 
 # List of settings that may be in string import notation.
@@ -35,7 +29,6 @@ IMPORT_STRINGS = (
     'JWT_ENCODE_HANDLER',
     'JWT_DECODE_HANDLER',
     'JWT_PAYLOAD_HANDLER',
-    'JWT_PAYLOAD_GET_USER_ID_HANDLER',
 )
 
 api_settings = APISettings(USER_SETTINGS, DEFAULTS, IMPORT_STRINGS)
