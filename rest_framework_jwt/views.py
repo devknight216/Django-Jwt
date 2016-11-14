ad80eeafe34a5f5aabe360cb815a4641f71ea2f9
devknight216 <devknight216@gmail.com>
2016-11-14 19:34:40 -0200

    Move JSONWebTokenAuthentication up in DEFAULT_AUTHENTICATION_CLASSES (fixes #282)

--- rest_framework_jwt/views.py
+++ rest_framework_jwt/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from datetime import datetime
 
 from .settings import api_settings
 from .serializers import (
@@ -58,15 +57,8 @@ class JSONWebTokenAPIView(APIView):
             user = serializer.object.get('user') or request.user
             token = serializer.object.get('token')
             response_data = jwt_response_payload_handler(token, user, request)
-            response = Response(response_data)
-            if api_settings.JWT_AUTH_COOKIE:
-                expiration = (datetime.utcnow() +
-                              api_settings.JWT_EXPIRATION_DELTA)
-                response.set_cookie(api_settings.JWT_AUTH_COOKIE,
-                                    response.data['token'],
-                                    expires=expiration,
-                                    httponly=True)
-            return response
+
+            return Response(response_data)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
